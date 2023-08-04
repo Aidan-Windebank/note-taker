c5e49d472c90f76e140ea445397e60411c6e3ad4
Aidan-Windebank <ajwindybanks@gmail.com>
2023-08-04 13:48:59 -0500

    changed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const html = require("./routes/html");
 const api = require("./routes/api")
 app.use(express.static('public'));
